8a2c53ee6311628171f03140a60cf03d0ed724cc
pathik-9902 <pathik.patel992002@gmail.com>
2025-09-13 23:47:54 +0530

    vite changed

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,16 +1,15 @@
-import { createHashRouter } from "react-router-dom";
-import MainLayout from "../layouts/MainLayout";
+import { createBrowserRouter } from "react-router-dom"
+import MainLayout from "../layouts/MainLayout"
 
 // Pages
-import Home from "../pages/Home/Home";
-import About from "../pages/About";
-import Services from "../pages/Services/Services";
-import Infrastructure from "../pages/Infrastructure";
-import Contact from "../pages/Contact";
-import Certificates from "../pages/Certificates";
+import Home from "../pages/Home/Home"
+import About from "../pages/About"
+import Services from "../pages/Services/Services"
+import Infrastructure from "../pages/Infrastructure"
+import Contact from "../pages/Contact"
+import Certificates from "../pages/Certificates"
 
-// Use HashRouter to prevent 404s on GitHub Pages
-export const router = createHashRouter([
+export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
@@ -19,8 +18,9 @@ export const router = createHashRouter([
       { path: "/about", element: <About /> },
       { path: "/services", element: <Services /> },
       { path: "/infrastructure", element: <Infrastructure /> },
-      { path: "/certificates", element: <Certificates /> },
       { path: "/contact", element: <Contact /> },
+      { path: "/certificates", element: <Certificates /> },
+
     ],
   },
-]);
+])
