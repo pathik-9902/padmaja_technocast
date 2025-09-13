09a2d610584ca3527f8cf41f586e023b7885040b
pathik-9902 <pathik.patel992002@gmail.com>
2025-09-13 23:57:23 +0530

    vite changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/padmaja_technocast/",
   server: {
-    host: '0.0.0.0', // Allows access from other devices on the network
-    port: 5173,      // Optional: explicitly set the port
+    host: "0.0.0.0", // Allows access from other devices on the network
+    port: 5173, // Optional: explicitly set the port
   },
-})
+});
