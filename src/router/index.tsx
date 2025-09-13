e0ee6a6e74c48aeec2718fdfc6a7b156407b9fb7
pathik-9902 <pathik.patel992002@gmail.com>
2025-09-14 01:37:17 +0530

    vite changed

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 
 // Pages
