8507ebda49c740b957174b4defef1a596a727333
pathik-9902 <pathik.patel992002@gmail.com>
2025-09-13 23:26:51 +0530

    vite changed

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import { createHashRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 
 // Pages
@@ -9,17 +9,23 @@ import Infrastructure from "../pages/Infrastructure";
 import Contact from "../pages/Contact";
 import Certificates from "../pages/Certificates";
 
-export const router = createHashRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/about", element: <About /> },
+        { path: "/services", element: <Services /> },
+        { path: "/infrastructure", element: <Infrastructure /> },
+        { path: "/certificates", element: <Certificates /> },
+        { path: "/contact", element: <Contact /> },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/about", element: <About /> },
-      { path: "/services", element: <Services /> },
-      { path: "/infrastructure", element: <Infrastructure /> },
-      { path: "/certificates", element: <Certificates /> },
-      { path: "/contact", element: <Contact /> },
-    ],
-  },
-]);
+    basename: "/padmaja_technocast", // 👈 this is the fix
+  }
+);
+ 
