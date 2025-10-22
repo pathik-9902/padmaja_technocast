b626c75fdfa2fa6460c3bd6cf1c00afc60fafd53
pathik-9902 <pathik.patel992002@gmail.com>
2025-10-22 19:52:31 +0530

    theme update

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,30 +1,25 @@
-import { createBrowserRouter } from "react-router-dom"
-import MainLayout from "../layouts/MainLayout"
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
 
 // Pages
-import Home from "../pages/Home/Home"
-import About from "../pages/About"
-import Services from "../pages/Services/Services"
-import Infrastructure from "../pages/Infrastructure"
-import Contact from "../pages/Contact"
-import Certificates from "../pages/Certificates"
+import Home from "../pages/Home/Home";
+import About from "../pages/About";
+import Services from "../pages/Services/Services";
+import Infrastructure from "../pages/Infrastructure";
+import Contact from "../pages/Contact";
+import Certificates from "../pages/Certificates";
 
-export const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <MainLayout />,
-      children: [
-        { path: "/", element: <Home /> },
-        { path: "/about", element: <About /> },
-        { path: "/services", element: <Services /> },
-        { path: "/infrastructure", element: <Infrastructure /> },
-        { path: "/contact", element: <Contact /> },
-        { path: "/certificates", element: <Certificates /> },
-      ],
-    },
-  ],
+export const router = createBrowserRouter([
   {
-    basename: "/padmaja_technocast", // fixes GitHub Pages routing
-  }
-)
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "services", element: <Services /> },
+      { path: "infrastructure", element: <Infrastructure /> },
+      { path: "contact", element: <Contact /> },
+      { path: "certificates", element: <Certificates /> },
+    ],
+  },
+]);
